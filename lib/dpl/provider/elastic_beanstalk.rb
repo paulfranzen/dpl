ac4e73aefab602002e3b5cb5e719d005aa186cf9
Paul Franzen <paulfranzen@gmail.com>
2014-08-28 14:55:10 -0400

    Update elastic_beanstalk.rb

--- lib/dpl/provider/elastic_beanstalk.rb
+++ lib/dpl/provider/elastic_beanstalk.rb
@@ -78,7 +78,8 @@ module DPL
         directory = Dir.pwd
         build_files = Dir.glob("**/*")
         zipfile_name = File.join(directory, archive_name)
-        puts ">>>> #{build_files.to_s} >>>>"
+        puts ">>>>>>>>>>>>>>> #{build_files.to_s} >>>>>>>>>>>>>>>"
+        puts ">>>>>>>>>>>>>>> #{files_to_pack.to_s} >>>>>>>>>>>>>>>"
         Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
           files_to_pack.each do |file|
             relative_archive_path = File.join(directory, '/')
